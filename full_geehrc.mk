ca624ac998b1082a666dd0354c42ecffc56ee43d
Abhinav1997 <abhinav.jhanwar.august2@gmail.com>
2015-10-09 02:23:34 +0200

    Set PRODUCT_RESTRICT_VENDOR_FILES to false

--- full_geehrc.mk
+++ full_geehrc.mk
@@ -38,7 +38,7 @@ PRODUCT_DEVICE := geehrc
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := AOSP on Geehrc
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_RESTRICT_VENDOR_FILES := true
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/lge/geehrc/device.mk)
